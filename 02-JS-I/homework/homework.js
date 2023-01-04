6d265bda3536f9ef65b7bfdfff085854c3fdbf1c
guillermocg89 <aldodanielcg@gmail.com>
2023-01-04 18:18:00 -0500

    recapitulacion

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -256,7 +256,9 @@ function esVocal(letra){
   if (letra === 'a' || letra === 'e' || letra === 'i' || letra === 'o' || letra === 'u' ){
     return 'Es vocal'
   }
+  else(){
     return 'Dato incorrecto'
+  }
 }
 
 
