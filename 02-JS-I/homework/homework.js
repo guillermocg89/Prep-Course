580d689f0dd0192b7a9d1a9e22269265a95e9c71
guillermocg89 <aldodanielcg@gmail.com>
2023-01-04 18:30:06 -0500

    terminamos JSI

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -256,9 +256,7 @@ function esVocal(letra){
   if (letra === 'a' || letra === 'e' || letra === 'i' || letra === 'o' || letra === 'u' ){
     return 'Es vocal'
   }
-  else(){
     return 'Dato incorrecto'
-  }
 }
 
 
